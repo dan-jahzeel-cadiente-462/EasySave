7d32bec4dbd6f4e298e448e0292a9e3ac06fc5ac
Dan Jahzeel Cadiente <dcadiente462@gmail.com>
2026-05-31 10:46:04 +0800

    fix: Symfony-Railway maintenance attempt 2

--- build_deps.sh
+++ build_deps.sh
@@ -5,7 +5,6 @@ set -e
 export APP_ENV=prod
 export DATABASE_URL=sqlite:///%kernel.project_dir%/var/data.db
 export APP_SECRET=dummy_secret_for_build
-export DEFAULT_URI=https://easysave.up.railway.app
 export MAILER_DSN=null://null
 export MESSENGER_TRANSPORT_DSN=null://null
 export GOOGLE_CLIENT_ID=dummy
